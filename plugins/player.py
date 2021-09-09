940fd5667a0437b3d1954a285bc75256c6e48134
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-09-09 11:03:00 +0700

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -1,6 +1,8 @@
 #MIT License
 
-#Copyright (c) 2021 SUBIN
+#Audiomusiccstreaming (Telegram Voice Chat Bot)
+#Credits @subinps, @dashezup and @AsmSafone
+#Copyright (C) brut-ctrl <https://github.com/brut-ctrl>
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
