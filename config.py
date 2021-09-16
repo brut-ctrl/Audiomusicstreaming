b2cbfeb8b839f2327e3f70db1a6ff46e2633e7ea
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-09-16 13:55:45 +0700

    Update config.py

--- config.py
+++ config.py
@@ -63,8 +63,8 @@ class Config:
     ADMIN = os.environ.get("ADMINS", '712147852 1408440765 1944787421 1820831401 1945910995 1909021805 1605366945 1880475785')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     API_ID = int(os.environ.get("API_ID", '7363937'))
-    CHAT = int(os.environ.get("CHAT", "-1001279520903"))
-    LOG_GROUP=os.environ.get("LOG_GROUP", "-1001279520903")
+    CHAT = int(os.environ.get("CHAT", "-1001394798993"))
+    LOG_GROUP=os.environ.get("LOG_GROUP", "-1001394798993")
     if LOG_GROUP:
         LOG_GROUP=int(LOG_GROUP)
     else:
