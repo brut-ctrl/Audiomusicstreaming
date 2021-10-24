e3dfb0ed7340d6212d124e643b3f201b83b74539
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-10-25 03:45:33 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -330,7 +330,7 @@ class Audiomusicstreaming(object):
         else:       
             pl = playlist[0]
             title = pl[1]
-        call = InputGroupCall(id=self.group_call.group_call, access_hash=self.group_call.group_call.access_hash)
+        call = InputGroupCall(id=self.group_call.group_call, access_hash=self.group_call.group_call)
         edit = EditGroupCallTitle(call=call, title=title)
         try:
             await self.group_call.client.send(edit)
